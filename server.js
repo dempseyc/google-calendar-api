245e95a33ed12c4bce44807ba68f90a7b096b74d
Craig Dempsey <craigdempsey@gmail.com>
2018-11-12 18:35:16 -0500

    check gh

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const app = express()
+// check github working
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 const request = require('request');
 const ical2json = require('ical2json');
