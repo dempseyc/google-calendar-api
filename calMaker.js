0344755fea29a834984c56c727d91e0e9c496ae9
Craig Dempsey <craigdempsey@gmail.com>
2018-12-09 01:31:01 -0500

    got tests working on exported object, move implementation details into calmaker.js

--- calMaker.js
+++ calMaker.js
@@ -1,7 +1,65 @@
-class CALMAKER {
-    methodForTesting (num1,num2) {
+const rrulestr = require('rrule').rrulestr;
+
+let CALMAKER = {
+
+    myNum: undefined,
+
+    vevent: undefined,
+    bucketDefinitions: undefined,
+
+    funcForTesting: (num1,num2) => {
         return num1+num2;
+    },
+
+    funcWithThisForTesting: (num) => {
+        this.myNum = num;
+        return this.myNum;
+    },
+
+    // setLatestDateFilter: (earliestdatefilter,tSpan) => {
+    //     // not implemented
+    //     // tSpan is obj with properties 'unit' and 'number'  like '4M'
+    //     return earliestdatefilter;
+    // },
+
+    extractRRULEobj: (rrule, eventdate) => {
+        let rruleObj = rrulestr(`${eventdate}\n${rrule}`);
+        return rruleObj;
+    },
+
+    currItemsRRextracted: (items, eventdate) => {
+        let rrextracted = items.map( (item) => {
+            if (item.RRULE) {
+                item.RRULE = extractRRULE(item.RRULE, eventdate);
+            }
+            return item;
+        })
+        return rrextracted;
+    },
+
+    filterByEarliest: (veventData, earliestdatefilter, now, latestdatefilter, returnCount) => {
+        // console.log(items, "in fd");
+        let currItems = veventData.filter( (item) => {
+            if(!item.RRULE && earliestdatefilter<=now) {
+                return false;
+            } else {
+                return true;
+            }
+        });
+        return currItems;
+    },  // end filterByEarliest
+
+    processData: (JSONdata) => {
+        let items = JSONdata.VCALENDAR[0].VEVENT; // should be array
+        //filter by earliest
+        //extrapolate from rrrules
+        //set up buckets by # limit
+        //return first bucket
+        let newVeventList = filterByEarliest(items);
+        JSONdata.VCALENDAR[0].VEVENT = newVeventList;
+        return JSONdata;
     }
+
 }
 
-module.exports = new CALMAKER();
+module.exports = CALMAKER;
--- test/test.js
+++ test/test.js
@@ -1,13 +1,19 @@
 const expect = require('chai').expect;
-// const CALMAKER = require('../calMaker').CALMAKER;
-const CALMAKER = require('../calMaker');
+const CALMAKER = require('../CALMAKER');
 
 
-describe('methForTesting()', function() {
+describe('funcForTesting()', function() {
     it('should return sum of two numbers', function() {
         let num1 = 1;
         let num2 = 2;
         let sum = num1 + num2;
-        expect(CALMAKER.methodForTesting(1,2)).to.be.equal(sum);
+        expect(CALMAKER.funcForTesting(1,2)).to.be.equal(sum);
+    });
+});
+
+describe('funcWithThisForTesting()', function() {
+    it('should return the num passed in', function() {
+        let num = 9;
+        expect(CALMAKER.funcWithThisForTesting(num)).to.be.equal(num);
     });
 });
